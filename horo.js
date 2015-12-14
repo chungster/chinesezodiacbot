775893e1d350e7fc8d04bd6af60ab31ed9fe313e
Chungster <say.no.to.sharkfin.soup@gmail.com>
2015-12-14 02:09:26 -0500

    updating file

--- horo.js
+++ horo.js
@@ -1,17 +1,25 @@
-var animal = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"];
-/* animals */
 
 module.exports = function (robot){ 
-	robot.respond(/year/i, function(msg){
-		year = res.match[1]
-		if animal is "rat" 
-			return msg.send(":rat:");
-	}
+	robot.hear(/birthyear(.*)/i, function(msg){
+			return msg.send("you are a "+ ":rat:")
+
+	});
 };
 
-
-// create function to get animal 
+var animal = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"];
 function getAnimal (year) {
 	var index = (year - 1984) % 12; 
 	return animal[index];	
 }
+
+			// return msg.send(":ox:")
+			// return msg.send(":tiger:")
+			// return msg.send(":rabbit:")
+			// return msg.send(":dragon:")
+			// return msg.send(":snake:")
+			// return msg.send(":horse:")
+			// return msg.send(":goat:")
+			// return msg.send(":monkey:")
+			// return msg.send(":rooster:")
+			// return msg.send(":dog:")
+			// return msg.send(":pig:")
