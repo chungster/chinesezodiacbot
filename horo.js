bf3c29772325134c8229139922b60a924a4a116b
Caroline Chung <say.no.to.sharkfin.soup@gmail.com>
2015-12-14 11:58:14 -0500

    Update horo.js

--- horo.js
+++ horo.js
@@ -1,25 +1,15 @@
 
 module.exports = function (robot){ 
-	robot.hear(/birthyear(.*)/i, function(msg){
-			return msg.send("you are a "+ ":rat:")
+	robot.hear(/year(.*)/i, function(msg){
+		var year = parseInt(msg.match[1]);
+		var animal = getAnimal(year);
+			return msg.send(":" + animal +":")
 
 	});
 };
 
 var animal = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"];
 function getAnimal (year) {
-	var index = (year - 1984) % 12; 
+	var index = (year - 1924) % 12; 
 	return animal[index];	
 }
-
-			// return msg.send(":ox:")
-			// return msg.send(":tiger:")
-			// return msg.send(":rabbit:")
-			// return msg.send(":dragon:")
-			// return msg.send(":snake:")
-			// return msg.send(":horse:")
-			// return msg.send(":goat:")
-			// return msg.send(":monkey:")
-			// return msg.send(":rooster:")
-			// return msg.send(":dog:")
-			// return msg.send(":pig:")
